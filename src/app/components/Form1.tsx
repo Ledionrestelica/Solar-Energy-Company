9b2f6a629820200712d28b4aba7d312f2789da1a
ledionres <ledionrestelica@icloud.com>
2024-11-24 18:39:18 +0100

    Fix

--- src/app/components/Form1.tsx
+++ src/app/components/Form1.tsx
@@ -56,7 +56,7 @@ const Form1 = () => {
       Phone: phone,
     };
 
-    fetch("/api/send-email", {
+    fetch("https://timi-form.vercel.app/api/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),

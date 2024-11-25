dd858fe94d92e5dd6b54fc72f35e186b96c714f5
ledionres <ledionrestelica@icloud.com>
2024-11-25 01:46:41 +0100

    fix api

--- src/app/components/Form1.tsx
+++ src/app/components/Form1.tsx
@@ -62,7 +62,7 @@ const Form1 = () => {
       Phone: phone,
     };
 
-    fetch("https://solprice.se/api/send-email", {
+    fetch("https://solpricer.se/api/send-email", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
